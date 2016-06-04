f0efc052f0fe4461acae6d6a82a7db2e5dadae27
Hana Lee <i@leehana.co.kr>
2016-06-04 22:44:33 +0900

    TDD 및 리팩토링 연습

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,77 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Hana Lee
+ * @since 2016-06-04 20:15
+ */
+public class GameTest {
+
+	private Game game;
+
+	@Before
+	public void setUp() throws Exception {
+		game = new Game();
+	}
+
+	@Test
+	public void canRoll() {
+		game.roll(0);
+	}
+
+	@Test
+	public void gutterGame() {
+		rollMany(0, 20);
+		assertThat(game.getScore(), is(0));
+	}
+
+	@Test
+	public void allOnes() {
+		rollMany(1, 20);
+		assertThat(game.getScore(), is(20));
+	}
+
+	@Test
+	public void oneSpare() {
+		rollSpare();
+		game.roll(3);
+		rollMany(17, 0);
+
+		assertThat(game.getScore(), is(16));
+	}
+
+	private void rollSpare() {
+		game.roll(5);
+		game.roll(5);
+	}
+
+	@Test
+	public void oneStrike() {
+		rollStrike();
+		game.roll(5);
+		game.roll(3);
+		rollMany(16, 0);
+		assertThat(game.getScore(), is(26));
+	}
+
+	private void rollStrike() {
+		game.roll(10);
+	}
+
+	@Test
+	public void perfectGame() {
+		rollMany(10, 10);
+		game.roll(10);
+		game.roll(10);
+		assertThat(game.getScore(), is(300));
+	}
+
+	private void rollMany(int pins, int frames) {
+		for (int i = 0; i < frames; i++) {
+			game.roll(pins);
+		}
+	}
+}
